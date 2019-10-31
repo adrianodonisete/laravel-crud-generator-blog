4572e13c6ca49040381c543dc68b283152d05a11
Adriano Machado <adrianodonisete@gmail.com>
2019-10-31 19:15:44 +0000

    create project and config InfyOm

---
+++ app/Models/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Product
+ * @package App\Models
+ * @version October 31, 2019, 7:08 pm UTC
+ *
+ * @property string name
+ * @property string body
+ * @property number code
+ */
+class Product extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'products';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'name',
+        'body',
+        'code'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'body' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required',
+        'body' => 'required',
+        'code' => 'required'
+    ];
+
+    
+}
---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class ProductRepository
+ * @package App\Repositories
+ * @version October 31, 2019, 7:08 pm UTC
+*/
+
+class ProductRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'code'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Product::class;
+    }
+}
